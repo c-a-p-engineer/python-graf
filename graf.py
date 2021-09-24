ae2892cb1b3c0f4b9b7d63886b0bde1fea1f3cd3
こぴぺたん <takahiro.inoue.0508+github@gmail.com>
2021-09-24 15:20:13 +0900

    コロナ日別感染者数動画作成

--- graf.py
+++ graf.py
@@ -1,56 +1,33 @@
 import requests
+import json
 import pandas as pd
 import bar_chart_race as bcr
- 
-# CSVデータをダウンロードする
-r = requests.get('https://covid.ourworldindata.org/data/ecdc/total_cases.csv')
-with open('covid19.csv', 'w') as f:
+import japanize_matplotlib
+
+# コロナの感染者を取得
+r = requests.get('https://opendata.corona.go.jp/api/Covid19JapanAll')
+with open('covid19_daily.json', 'w') as f:
     f.write(r.text)
- 
-# DataFrameにする
-raw_df = pd.read_csv('covid19.csv', index_col='date', parse_dates=['date'])
- 
-# 主要な国を抽出、10行置きのDataFrameを抽出
-df = raw_df.loc[:, ["Brazil",
-                    "China",
-                    "France",
-                    "India",
-                    "Iran",
-                    "Italy",
-                    "Japan",
-                    "Spain",
-                    "United Kingdom",
-                    "United States"]
-                ].dropna()[::3]
- 
+
+# JSONを読み込む
+data = json.load(open('covid19_daily.json'))
+
+# JSONをデータフレームにする
+df = pd.DataFrame(data["itemList"])
+
+# 整形
+# 10行ごとにデータ変換
+df = df.pivot_table(
+    index='date',
+    columns='name_jp',
+    values='npatients').dropna()[::10]
+
 # アニメーションをmp4で保存する
 bcr.bar_chart_race(
     df=df,
-    filename='covid19.mp4',
+    filename='covid19_daily.mp4',
+    title='COVID-19 都道府県別感染者数',
     orientation='h',
     sort='desc',
-    n_bars=6,
-    fixed_order=False,
-    fixed_max=True,
-    steps_per_period=10,
-    interpolate_period=False,
-    label_bars=True,
-    bar_size=.95,
-    period_label={'x': .99, 'y': .25, 'ha': 'right', 'va': 'center'},
-    period_fmt='%B %d, %Y',
-    period_summary_func=lambda v, r: {'x': .99, 'y': .18,
-                                      's': f'Total confirmed cases: {v.nlargest(6).sum():,.0f}',
-                                      'ha': 'right', 'size': 8},
-    period_length=500,
-    figsize=(5, 3),
-    dpi=144,
-    cmap='dark12',
-    title='COVID-19 Total confirmed cases by Country',
-    title_size='',
-    bar_label_size=7,
-    tick_label_size=7,
-    scale='linear',
-    writer=None,
-    fig=None,
-    bar_kwargs={'alpha': .7},
-    filter_column_colors=False)
+    n_bars=10,
+)
